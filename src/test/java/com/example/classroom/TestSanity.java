c6100f56034a9dfd03c046e3ac27a8f57ca406ba
Arepo00 <kamalbentoua@gmail.com>
2025-05-10 22:16:00 +0100

    hhhhh

---
+++ src/test/java/com/example/classroom/TestSanity.java
@@ -0,0 +1,11 @@
+package com.example.classroom;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestSanity {
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
